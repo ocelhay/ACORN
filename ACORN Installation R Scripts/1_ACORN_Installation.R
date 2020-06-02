3a0fb262ec8c93ea54e4edcbebed385df53748d2
Olivier <olivier.celhay@gmail.com>
2020-06-02 10:32:58 -0700

    Align package requirements

--- ACORN Installation R Scripts/1_ACORN_Installation.R
+++ ACORN Installation R Scripts/1_ACORN_Installation.R
@@ -7,19 +7,7 @@ pacman::p_load(curl, devtools, remotes, utils)
 pacman::p_load(bsplus, data.table, digest, DT, flexdashboard, flextable,
                foreign, grid, gridExtra, highcharter, knitr, jsonlite,  lubridate,  markdown, openssl, 
                purrr,  pushbar,  RColorBrewer, readxl, remotes, shiny, shinyBS, 
-               shinyhelper, shinythemes, tidyverse, timevis, vov)
-
-
-# We need digest v. 0.6.23
-if("digest" %in% rownames(installed.packages())) {
-  if(pacman::p_version("digest") != "0.6.23") {
-    remove.packages("digest")
-  }
-}
-
-if(! "digest" %in% rownames(installed.packages())) {
-  devtools::install_github("https://github.com/eddelbuettel/digest/commit/b3a805b88b3414a7ffc85c1e9873a3c6ebd1b9e9")
-}
+               shinyhelper, shinythemes, shinyWidgets, tidyverse, timevis, vov)
 
 print("Step 1a completed")
 
