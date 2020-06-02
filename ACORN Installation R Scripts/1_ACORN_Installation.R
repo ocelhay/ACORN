f33ee7acdd6854a6fd35adee3f7eecdc2226c437
Olivier <olivier.celhay@gmail.com>
2020-06-02 10:42:45 -0700

    set option for CRAN repo

--- ACORN Installation R Scripts/1_ACORN_Installation.R
+++ ACORN Installation R Scripts/1_ACORN_Installation.R
@@ -1,13 +1,23 @@
 # Install all required packages ----
 
+
+# set default repository
+r <- getOption("repos")
+r["CRAN"] <- "https://cran.rstudio.com/"
+options(repos = r)
+
+
 # check if these packages are installed, install them otherwise
 if (!require('pacman')) install.packages('pacman', quiet = TRUE)
 pacman::p_load(curl, devtools, remotes, utils)
 
-pacman::p_load(bsplus, data.table, digest, DT, flexdashboard, flextable,
-               foreign, grid, gridExtra, highcharter, knitr, jsonlite,  lubridate,  markdown, openssl, 
-               purrr,  pushbar,  RColorBrewer, readxl, remotes, shiny, shinyBS, 
-               shinyhelper, shinythemes, shinyWidgets, tidyverse, timevis, vov)
+
+# Packages required in the App
+pacman::p_load(bsplus, data.table, DT, flexdashboard, flextable, foreign, grid, gridExtra, highcharter, 
+               knitr, jsonlite,  lubridate,  markdown, openssl,  pushbar,  RColorBrewer, readxl, shiny, shinyBS, 
+               shinyhelper, shinythemes, shinyWidgets, tidyverse, tools, timevis, vov)
+
+pacman::p_load(remotes)
 
 print("Step 1a completed")
 
