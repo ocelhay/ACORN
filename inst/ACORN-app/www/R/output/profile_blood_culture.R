f81b6649d132f9fe0db4cf7828f75ac850fe4280
Olivier <olivier.celhay@gmail.com>
2020-06-12 11:47:21 -0700

    fix denominator gauge

--- inst/ACORN-app/www/R/output/profile_blood_culture.R
+++ inst/ACORN-app/www/R/output/profile_blood_culture.R
@@ -8,7 +8,7 @@ output$profile_blood_culture_gauge <- renderGauge({
     slice(1) %>%
     nrow()
   
-  total <- patient() %>% nrow()
+  total <- patient_filter() %>% nrow()
   
   gauge(n_blood_culture, min = 0, max = total, abbreviate = FALSE, gaugeSectors(colors = "#e31a1c"))
 })
@@ -23,7 +23,7 @@ output$profile_blood_culture_pct <- renderText({
     slice(1) %>%
     nrow()
   
-  total <- patient() %>% nrow()
+  total <- patient_filter() %>% nrow()
   
   paste(br(),br(), h4(paste0(round(100*n_blood_culture/total, 1), "%")), span("of patients with blood culture."))
 })
